12518fcff98349e9fb2c7fefde3569ba913bab3a
Danielgb1217 <Danielgb1217@gmail.com>
2021-02-17 20:56:30 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace sales_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            // products instances 
+            Product product1 = new Product("Computer", 3000000);
+            Product product2 = new Product("laptop", 2000000);
+            Product product3 = new Product("desk", 500000);
+
+            //sellers instances
+            Seller seller1 = new Seller("Daniel", 27);
+            Seller seller2 = new Seller("Andres", 30);
+            Seller seller3 = new Seller("Alejandra", 24);
+
+            //sales instances
+            Sale sale1 = new Sale(product1, seller2, "computer hp");
+            Sale sale2 = new Sale(product2, seller1, "laptop  Asus");
+            Sale sale3 = new Sale(product3, seller3, "white desk");
+
+
+
+        }
+
+     /*   public static int GetTotalSales()
+        {
+            return product1.GetTotalSales
+        }
+     */
+
+    }
+}
